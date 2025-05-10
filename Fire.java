1182480452bfd637a4420829242c67edd5eebef5
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-05-10 10:43:05 +0600

    Update Fire.java

--- Fire.java
+++ Fire.java
@@ -1,28 +1,29 @@
 import java.awt.*;
 import javax.swing.ImageIcon;
-
-public class Fire {
+public class Fire{
     int x,y;
     int width=30;
     int height=30;
     int speed=5;
+    //int speedX=3;
+    int count=0;
     Image firePic;
     boolean active=true;
-
-    public Fire(int startX,int startY) {
+    public Fire(int startX,int startY)
+    {
         this.x=startX;
         this.y=startY;
-
         try {
-            firePic=new ImageIcon("C:\\Users\\hp\\OneDrive\\Desktop\\git clone\\Game Project\\src\\image\\fireball.png").getImage();
-        } catch(Exception e) {
-            System.out.println("Error loading fire image: " + e);
+            firePic = new ImageIcon("C:\\Users\\hp\\OneDrive\\Desktop\\Game Project\\src\\image\\fireball.png").getImage();
+        }catch(Exception e) {
+            System.out.println("Error loading fire image : " + e);
         }
     }
-
-    public void update(int screenHeight) {
+    public void update(int screenHeight)
+    {
         y+=speed;
-        if (y>screenHeight)
+       // x+=speedX;
+        if(y>screenHeight)
             active=false;
     }
 }
