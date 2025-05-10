a4e9ecdfe8b5dccf26ce96087e24b80a6ee41310
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-05-10 10:41:10 +0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,42 +1,38 @@
 import java.awt.*;
-
-public class Player {
-    int x=300;
-    int y=300;
+public class Player{
+    int x=300,y=300;
     int width=80;
     int height=80;
-    int speedX=0;
-    int speedY=0;
+    int speedX=0,speedY=0;
     boolean jumping=false;
     int jumpHeight=0;
     int jumpSpeed=10;
     int maxJump=100;
     Image playerPic;
-    int groundY=500;
-
-    void update(int screenWidth,int screenHeight) {
+    int groundY=400;
+    void update(int screenWidth,int screenHeight)
+    {
         x=x+speedX;
         y=y+speedY;
-
-        if(x<0) {
+        if(x<0){
             x=0;
         }
-        if (x>screenWidth-width) {
+        if(x>screenWidth-width){
             x=screenWidth-width;
         }
-        if (y<0) {
+        if(y<0){
             y=0;
         }
-        if(y>screenHeight-height) {
+        if(y>screenHeight-height){
             y=screenHeight-height;
         }
-        if(jumping==true) {
-            if(jumpHeight<maxJump) {
+        if(jumping==true){
+            if(jumpHeight<maxJump){
                 y=y-jumpSpeed;
                 jumpHeight=jumpHeight+jumpSpeed;
             }else{
                 y=y+jumpSpeed;
-                if(y>=groundY) {
+                if(y>=groundY){
                     y=groundY;
                     jumping=false;
                 }
