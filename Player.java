b7f195e6233058c72e6fb58b787f12378328d3a0
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-05-31 22:24:37 +0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,42 +1,56 @@
 import java.awt.*;
-public class Player{
+
+public class Player {
     int x=300,y=300;
-    int width=80;
-    int height=80;
-    int speedX=0,speedY=0;
+    int width=70;
+    int height=70;
+    int speedX=0, speedY=0;
     boolean jumping=false;
-    int jumpHeight=0;
-    int jumpSpeed=10;
-    int maxJump=100;
+    int gravity=1; 
+    int jumpVelocity=-15;  
     Image playerPic;
-    int groundY=400;
-    void update(int screenWidth,int screenHeight)
-    {
+    int groundY=500; 
+
+    void update(int screenWidth,int screenHeight) {
         x=x+speedX;
-        y=y+speedY;
-        if(x<0){
+        
+        if (jumping) {
+            speedY+=gravity;
+            y+=speedY;
+            if (y>=groundY) {
+                y=groundY;
+                jumping=false;
+                speedY=0;
+            }
+        } else {
+            y+=speedY;
+        }
+
+        if (x<0) {
             x=0;
         }
-        if(x>screenWidth-width){
+        if (x>screenWidth-width) {
             x=screenWidth-width;
         }
-        if(y<0){
+        if (y<0) {
             y=0;
-        }
-        if(y>screenHeight-height){
-            y=screenHeight-height;
-        }
-        if(jumping==true){
-            if(jumpHeight<maxJump){
-                y=y-jumpSpeed;
-                jumpHeight=jumpHeight+jumpSpeed;
-            }else{
-                y=y+jumpSpeed;
-                if(y>=groundY){
-                    y=groundY;
-                    jumping=false;
-                }
+            if (jumping) {
+                speedY=0; 
             }
+        }
+        if (y>screenHeight-height) {
+            y=screenHeight-height;
+            if (jumping) {
+                jumping=false;
+                speedY=0;
+            }
+        }
+    }
+
+    void jump() {
+        if (!jumping && y>=groundY-5) {  
+            jumping=true;
+            speedY=jumpVelocity;  
         }
     }
 }
